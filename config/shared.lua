1c036f0468790be8a5643ce6d7e73c89f10d97f9
Bama94 <tegober94@gmail.com>
2026-04-14 23:17:03 -0500

    updated

--- config/shared.lua
+++ config/shared.lua
@@ -1,7 +1,9 @@
 return {
     boxLocations = {
-        { coords = vec4(2423.41, 3099.31, 47.15, 250.75), model = `mystery_box2`, openedModel = `mystery_box2_open`, empty = `empty_mysterybox_location`},
-        { coords =  vec4(2412.21, 3089.64, 47.22, 158.87), model = `mystery_box2`, openedModel = `mystery_box2_open`, empty = `empty_mysterybox_location`},
+        { coords = vec4(2703.07, 2774.67, 36.88, 215.7), model = `mystery_box2`, openedModel = `mystery_box2_open`, empty = `empty_mysterybox_location`},
+        { coords = vec4(3599.02, 3652.33, 40.34, 351.23), model = `mystery_box2`, openedModel = `mystery_box2_open`, empty = `empty_mysterybox_location`},
+        { coords = vec4(2510.29, 3788.79, 49.87, 270.0), model = `mystery_box2`, openedModel = `mystery_box2_open`, empty = `empty_mysterybox_location`},
+        { coords = vec4(1554.97, 3600.51, 37.78, 34.72), model = `mystery_box2`, openedModel = `mystery_box2_open`, empty = `empty_mysterybox_location`},
     },
     account = 'cash',
     price = 950,
